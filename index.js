ed11efb68bdaac077737dbd51396825d1d4a958a
ddr14 <david.dryburgh@sky.uk>
2020-06-29 17:19:34 +0100

    created some middleware

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import favicon from "serve-favicon";
+import path from "path";
+
 import data from "./data/data.json";
 
 const app = express();
@@ -7,22 +10,46 @@ const PORT = 3000;
 // this is for the public folder on path /
 app.use(express.static("public"));
 
+// Method to use JSON
+//app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/* Code for a basic proxy. May cause security concerns, make sur eyou are using best practice for security
+  app.set("trust proxy", "loopback");
+*/
+
 // this is for images folder on path /images
 app.use("/images", express.static("images"));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 app.get("/", (req, res) => {
   // get data first
   res.json(data);
 });
 
+// JSON data
+// {"hello": "JSON is cool"}
+// URLEncoded data
+// hello=URLEncoded+is+cool
+
+app.post("/newItem", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
 //using next() will allow the second block of code to run. This can be done multiple times. Response can only be used once
 app.get(
   "/item/:id",
   (req, res, next) => {
+    // this is the middleware that pulls the data
     console.log(req.params.id);
     let user = Number(req.params.id);
     console.log(user);
     console.log(data[user]);
+    // middleware that uses the request object
+    console.log(`Request from: ${req.originalUrl}`);
+    console.log(`Request type: ${req.method}`);
+    // everything above is middleware
     res.send(data[user]);
     next();
   },
@@ -34,10 +61,11 @@ app.get(
 app
   .route("/item")
   .get((req, res) => {
+    throw new Error();
     //res.download("images/rocket.jpg");
     //res.redirect("http://www.linkedin.com");
     //res.end();
-    res.send(`a get request with /item route on port ${PORT}`);
+    //res.send(`a get request with /item route on port ${PORT}`);
   })
   .put((req, res) => {
     res.send(`a put request with /item route on port ${PORT}`);
@@ -46,7 +74,13 @@ app
     res.send(`a delete request with /item route on port ${PORT}`);
   });
 
+// Error handling function
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send(`Red alert! Red alert!: ${err.stack}`);
+});
+
 app.listen(PORT, () => {
   console.log(`Your server is running on port ${PORT}`);
-  console.log(data);
+  //console.log(data);
 });
